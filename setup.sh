53c83e2c73ab1e75fcd8d150c198cff89105139b
binl233 <835741923@qq.com>
2026-02-19 16:12:46 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y clang bpfcc-tools llvm
 sudo apt install -y libc6-dev-i386 libjansson-dev libelf-dev linux-tools-common
-sudo apt install -y linux-headers-$(uname -r) linux-tools-$(uname -r) gh liblog4c-dev iperf3
-gh auth login
+sudo apt install -y linux-headers-$(uname -r) linux-tools-$(uname -r) liblog4c-dev iperf3
 
 echo Install libbpf v1.3.0
 
